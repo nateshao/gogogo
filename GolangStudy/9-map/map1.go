f0b5a091c7d5a02a301f34c0ee8b0a218d87fe0b
nateshao <shaotongjie@gmail.com>
2021-04-24 20:13:13 +0800

    map

--- GolangStudy/9-map/map1.go
+++ GolangStudy/9-map/map1.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+
+	var myMap1 map[string]string
+
+	if myMap1 == nil {
+		fmt.Println("myMap1 is nil..")
+	}
+	// 开辟空间
+	myMap1 = make(map[string]string, 10)
+	// 赋值
+	myMap1["one"] = "java"
+	myMap1["two"] = "python"
+	myMap1["three"] = "Go"
+	fmt.Println(myMap1)
+
+	fmt.Println("=========第二种声明方式==========")
+	myMap2 := make(map[int]string)
+	myMap2[1] = "java"
+	myMap2[2] = "c++"
+	myMap2[3] = "rust"
+	fmt.Println(myMap2)
 
 }
