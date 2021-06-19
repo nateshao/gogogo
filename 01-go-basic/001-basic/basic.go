30bd53eee08ce0b42c91cc870e0126ad08c083b5
nateshao <shaotongjie@gmail.com>
2021-06-19 11:56:53 +0800

    并行与并发

---
+++ 01-go-basic/001-basic/basic.go
@@ -0,0 +1,105 @@
+/**
+ * @date Created by 邵桐杰 on 2021/6/19 11:44
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ */
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
+
+var (
+	aa = 3
+	ss = "kkk"
+	bb = true
+)
+
+func variableZeroValue() {
+	var a int
+	var s string
+	fmt.Printf("%d %q\n", a, s)
+}
+
+func variableInitialValue() {
+	var a, b int = 3, 4  // int 这里可以省略 因为Go语言会识别到
+	var s string = "abc" // string 这里可以省略 因为Go语言会识别到
+	fmt.Println(a, b, s)
+}
+
+func variableTypeDeduction() {
+	var a, b, c, s = 3, 4, true, "def"
+	fmt.Println(a, b, c, s)
+}
+
+func variableShorter() {
+	a, b, c, s := 3, 4, true, "def"
+	b = 5
+	fmt.Println(a, b, c, s)
+}
+
+func euler() {
+	fmt.Printf("%.3f\n",
+		cmplx.Exp(1i*math.Pi)+1)
+}
+
+func triangle() {
+	var a, b int = 3, 4
+	fmt.Println(calcTriangle(a, b))
+}
+
+func calcTriangle(a, b int) int {
+	var c int
+	c = int(math.Sqrt(float64(a*a + b*b)))
+	return c
+}
+
+func consts() {
+	const (
+		filename = "abc.txt"
+		a, b     = 3, 4
+	)
+	var c int
+	c = int(math.Sqrt(a*a + b*b))
+	fmt.Println(filename, c)
+}
+
+func enums() {
+	const (
+		cpp = iota
+		_
+		python
+		golang
+		javascript
+	)
+
+	const (
+		b = 1 << (10 * iota)
+		kb
+		mb
+		gb
+		tb
+		pb
+	)
+
+	fmt.Println(cpp, javascript, python, golang)
+	fmt.Println(b, kb, mb, gb, tb, pb)
+}
+
+func main() {
+	fmt.Println("Hello world")
+	variableZeroValue()
+	variableInitialValue()
+	variableTypeDeduction()
+	variableShorter()
+	fmt.Println(aa, ss, bb)
+
+	euler()
+	triangle()
+	consts()
+	enums()
+}
