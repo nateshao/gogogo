a361be2f44192da494c8ff12526d5b93edd6546c
nateshao <shaotongjie@gmail.com>
2021-07-20 20:57:39 +0800

    并行与并发

--- 01-go-basic/001-basic/6-3strings.go
+++ 01-go-basic/001-basic/6-3strings.go
@@ -15,31 +15,31 @@ import (
 
 func main() {
 	s := "Yes我爱慕课网!" // UTF-8
-	fmt.Println(s)
+	fmt.Println(s)   // Yes我爱慕课网!
 
 	for _, b := range []byte(s) {
-		fmt.Printf("%X ", b)
+		fmt.Printf("%X ", b) // 59 65 73 E6 88 91 E7 88 B1 E6 85 95 E8 AF BE E7 BD 91 21
 	}
 	fmt.Println()
 
 	for i, ch := range s { // ch is a rune
-		fmt.Printf("(%d %X) ", i, ch)
+		fmt.Printf("(%d %X) ", i, ch) // (0 59) (1 65) (2 73) (3 6211) (6 7231) (9 6155) (12 8BFE) (15 7F51) (18 21)
 	}
 	fmt.Println()
 
 	fmt.Println("Rune count:",
-		utf8.RuneCountInString(s))
+		utf8.RuneCountInString(s)) // Rune count: 9
 
 	bytes := []byte(s)
 	for len(bytes) > 0 {
 		ch, size := utf8.DecodeRune(bytes)
 		bytes = bytes[size:]
-		fmt.Printf("%c ", ch)
+		fmt.Printf("%c ", ch) // Y e s 我 爱 慕 课 网 !
 	}
 	fmt.Println()
 
 	for i, ch := range []rune(s) {
-		fmt.Printf("(%d %c) ", i, ch)
+		fmt.Printf("(%d %c) ", i, ch) // (0 Y) (1 e) (2 s) (3 我) (4 爱) (5 慕) (6 课) (7 网) (8 !)
 	}
 	fmt.Println()
 }
