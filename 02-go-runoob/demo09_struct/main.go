991e6be873fbb781c219e0611105a545f3ee810c
nateshao <shaotongjie@gmail.com>
2022-04-18 15:20:09 +0800

    并行与并发

--- 02-go-runoob/demo09_struct/main.go
+++ 02-go-runoob/demo09_struct/main.go
@@ -14,4 +14,6 @@ func main() {
 	fmt.Println(Books{"千羽的编程时光", "千羽", "Go语言实战", 12})
 	// 也可以使用 key => value 格式
 	fmt.Println(Books{title: "千羽的编程时光", author: "千羽", subject: "Go开发实战", book_id: 12})
+	// 忽略的字段为 0 或 空
+	fmt.Println(Books{title: "Go 语言", author: "www.nateshao.cn"})
 }
