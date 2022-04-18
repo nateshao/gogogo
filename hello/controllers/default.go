ee1d8b89e69bba3df2809caabbe5cfe475d129f0
nateshao <shaotongjie@gmail.com>
2022-04-18 22:32:47 +0800

    beego

--- hello/controllers/default.go
+++ hello/controllers/default.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"hello/models"
 )
 
 type MainController struct {
@@ -9,7 +11,30 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+
+	o := orm.NewOrm()
+	user := models.User{}
+	user.Name = "111"
+	user.Username = "千羽"
+	user.Password = "1234"
+	_, err := o.Insert(&user)
+	if err != nil {
+		beego.Info("插入失败", err)
+		return
+	}
+	//c.Data["Website"] = "beego.me"
+	//c.Data["Email"] = "astaxie@gmail.com"
+	//c.TplName = "index.tpl"
+	c.Data["data"] = "千羽的编程时光"
+	c.TplName = "test.html"
+
+}
+
+func (c *MainController) Post() {
+	//c.Data["Website"] = "beego.me"
+	//c.Data["Email"] = "astaxie@gmail.com"
+	//c.TplName = "index.tpl"
+	c.Data["data"] = "Go开发的编程时光"
+	c.TplName = "test.html"
+
 }
--- hello/go.mod
+++ hello/go.mod
@@ -6,9 +6,10 @@ require github.com/astaxie/beego v1.12.3
 
 require (
 	github.com/OwnLocal/goes v1.0.0 // indirect
+	github.com/beego/beego/v2 v2.0.2 // indirect
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
 	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726 // indirect
 	github.com/siddontang/ledisdb v0.0.0-20181029004158-becf5f38d373 // indirect
 	github.com/smartystreets/goconvey v1.6.4
-	golang.org/x/tools v0.0.0-20200117065230-39095c1d176c // indirect
 )
