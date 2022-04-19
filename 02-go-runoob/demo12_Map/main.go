de6d7bb340ee41fa046511bc8828c3a8b5441574
nateshao <shaotongjie@gmail.com>
2022-04-19 21:35:51 +0800

    并行与并发

---
+++ 02-go-runoob/demo12_Map/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+/**
+Go 语言Map(集合)
+Map 是一种无序的键值对的集合。Map 最重要的一点是通过 key 来快速检索数据，key 类似于索引，指向数据的值。
+
+Map 是一种集合，所以我们可以像迭代数组和切片那样迭代它。不过，Map 是无序的，我们无法决定它的返回顺序，这是因为 Map 是使用 hash 表来实现的。
+*/
+func main() {
+
+	var countryCapitalMap map[string]string /*创建集合 */
+	countryCapitalMap = make(map[string]string)
+
+	/* map插入key - value对,各个国家对应的首都 */
+	countryCapitalMap["France"] = "巴黎"
+	countryCapitalMap["Italy"] = "罗马"
+	countryCapitalMap["Japan"] = "东京"
+	countryCapitalMap["India "] = "新德里"
+	for country := range countryCapitalMap {
+		fmt.Println(country, "首都是", countryCapitalMap[country])
+	}
+	/*查看元素在集合中是否存在 */
+	capital, ok := countryCapitalMap["American"] /*如果确定是真实的,则存在,否则不存在 */
+	/*fmt.Println(capital) */
+	/*fmt.Println(ok) */
+	if ok {
+		fmt.Println("American 的首都是", capital)
+	} else {
+		fmt.Println("American 的首都不存在")
+	}
+}
--- beego-blog/main.go
+++ beego-blog/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"github.com/astaxie/beego"
 	_ "beego-blog/routers"
+	"github.com/astaxie/beego"
 )
 
 func main() {
+	beego.AddFuncMap("ShowNextPage", getNextPage)
+	beego.AddFuncMap("ShowLastPage", getLastPage)
 	beego.Run()
+}
 
+func getNextPage(pageindex int) int {
+	pageindex++
+	return pageindex
+}
+func getLastPage(pageindex int) int {
+	if pageindex--; pageindex < 0 {
+		pageindex = 0
+	}
+	return pageindex
 }
